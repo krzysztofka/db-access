2a08c81689113e230d0c7fcbbf268504fb8f651d
e-kfkk <kalisiakk@gmail.com>
2016-05-13 15:00:31 +0200

    removed purge from repo

--- src/test/java/com/kali/dbaccess/TransactionExecutionTimeIT.java
+++ src/test/java/com/kali/dbaccess/TransactionExecutionTimeIT.java
@@ -1,39 +1,61 @@
 package com.kali.dbaccess;
 
 import com.kali.dbaccess.generator.DatabasePopulator;
+import com.kali.dbaccess.generator.GenerationContext;
 import com.kali.dbaccess.generator.InMemoryGenerationContext;
-import org.junit.Test;
+import com.kali.dbaccess.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class TransactionExecutionTimeIT extends AbstractSpringIT {
 
     private static final int QUANTITY = 10000;
 
+    private static final int REPETITIONS = 1000;
+
     @Autowired
     @Qualifier("productsPopulator")
     private DatabasePopulator populator;
 
-    @Test
-    public void testOneTransactionPerDataSet() {
+    @Autowired
+    private ProductRepository repository;
+
+    @Autowired
+    private JdbcTemplate template;
+
+    //@Test
+    public void testTransactionsExecutionTime() {
+        long sum = 0;
+        for (int i = 0; i < REPETITIONS; i++) {
+            sum += getGenerationTimeForSingleTransaction();
+        }
+
+        System.out.println("### Duration: " + (sum / REPETITIONS));
+    }
+
+    private long getGenerationTimeForSingleTransaction() {
         long start = System.currentTimeMillis();
-        InMemoryGenerationContext context = new InMemoryGenerationContext();
+
+        GenerationContext context = new InMemoryGenerationContext();
         populator.populate(context, QUANTITY);
 
         long end = System.currentTimeMillis();
-        System.out.println("testOneTransactionPerDataSet: " + (end - start) + " MilliSeconds");
+        System.out.println((end - start) + " MilliSeconds");
+        return end - start;
+
     }
 
-    @Test
-    public void testTransactionsPerEachProduct() {
+    private long getGenerationTimeForTransactionPerItem() {
         long start = System.currentTimeMillis();
 
-        InMemoryGenerationContext context = new InMemoryGenerationContext();
+        GenerationContext context = new InMemoryGenerationContext();
         for (int i = 0; i < QUANTITY; i++) {
             populator.populate(context, 1);
         }
 
         long end = System.currentTimeMillis();
-        System.out.println("testOneTransactionPerItem: " + (end - start) + " MilliSeconds");
+        System.out.println((end - start) + " MilliSeconds");
+        return end - start;
     }
 }
